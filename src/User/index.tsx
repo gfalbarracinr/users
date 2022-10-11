f458df03275e8335e1c0c865a2cf1ddcfce18d52
giovanny_albarracin <giovanny_albarracin@epam.com>
2022-10-11 16:58:04 -0500

    adding behavior

--- src/User/index.tsx
+++ src/User/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './user.css';
 import UserType from '../Types/UserType';
 
 const User: React.FunctionComponent<UserType> = ({ name, age }: UserType) => {
   return (
-    <article>
+    <article className="user-container">
       <p>{name}</p>
       <p>{age}</p>
     </article>
